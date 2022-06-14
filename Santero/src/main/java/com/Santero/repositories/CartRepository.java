e7e75e179abcdc1ffe2e035b4afc54becde8cd94
Franco Lamberti <franco_lamberti_02@yahoo.com.ar>
2022-06-14 11:22:35 -0300

    Reset

--- Santero/src/main/java/com/Santero/repositories/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Santero.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Santero.entities.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, String>{
-
-}
